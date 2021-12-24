e220417806ef6b7af8ca56b6e6a8870d8c52852b
tomioka-k <tomioka-k@tajima.co.jp>
2021-12-24 17:19:07 +0900

    #9 create post

--- routers/schemas.py
+++ routers/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class UserBase(BaseModel):
@@ -12,4 +13,31 @@ class UserDispllay(BaseModel):
     email: str
 
     class Config:
+        orm_mode = True
+
+
+class PostBase(BaseModel):
+    image_url: str
+    image_url_type: str
+    caption: str
+    creater_id: int
+
+
+# For PostDisplay
+class User(BaseModel):
+    username: str
+
+    class Config():
+        orm_mode = True
+
+
+class PostDisplay(BaseModel):
+    id: int
+    image_url: str
+    image_url_type: str
+    caption: str
+    timestamp: datetime
+    user: User
+
+    class Config():
         orm_mode = True
