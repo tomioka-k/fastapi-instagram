12097a9375f33a1f21468d32c904fff66124abf7
tomioka-k <tomioka-k@tajima.co.jp>
2021-12-24 16:00:25 +0900

    #7 hash user password

--- db/db_user.py
+++ db/db_user.py
@@ -1,13 +1,14 @@
 from routers.schemas import UserBase
 from sqlalchemy.orm.session import Session
 from .models import DbUser
+from db.hashing import Hash
 
 
 def create_user(db: Session, request: UserBase):
     new_user = DbUser(
         username=request.username,
         email=request.email,
-        password=request.password  # need hash
+        password=Hash.bcrypt(request.password)
     )
     db.add(new_user)
     db.commit()
